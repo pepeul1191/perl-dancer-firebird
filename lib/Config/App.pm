3b422dfe1f1e891ac4870d844c7ea8bd908b5c95
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-04 19:44:41 -0500

    Insert a una tabla de prueba

--- lib/Config/App.pm
+++ lib/Config/App.pm
@@ -1,5 +1,7 @@
 package Config::App;
 use Dancer2;
+use Try::Tiny;
+use Model::Prueba;
 
 our $VERSION = '0.1';
 
@@ -9,6 +11,45 @@ hook before => sub {
 
 get '/' => sub {
   template 'index' => { 'title' => 'Gestion::App' };
+};
+
+get '/db/listar' => sub {
+  my $model= 'Model::Prueba';
+  my $prueba= $model->new();
+  try {
+    my  @rpta = $prueba->listar();
+    return to_json \@rpta;
+  }
+  catch {
+    my %rpta = ();
+    $rpta{'tipo_mensaje'} = "error";
+    $rpta{'mensaje'} = 'Se ha producido un error en listar la tabla de pruebas';
+    my @temp = ('Se ha producido un error en listar la tabla de pruebas', '' . $_);
+    $rpta{'mensaje'} = [@temp];
+    return to_json \%rpta;
+  };
+};
+
+get 'db/crear' => sub {
+  my %rpta = ();
+  try {
+    my $nombres = 'José Jesús';
+    my $paterno = 'Valdivia';
+    my $materno = 'Caballero';
+    my $correo = 'pepe.valdivia.caballero@gmail.com';
+    my $model = 'Model::Prueba';
+    my $prueba = $model->new();
+    $rpta{'id'} = $prueba->crear($nombres, $paterno, $materno, $correo);
+  } 
+  catch {
+    #warn "got dbi error: $_";
+    $rpta{'tipo_mensaje'} = "error";
+    $rpta{'mensaje'} = "Se ha producido un error en guardar la tabla de pruebas";
+    my @temp = ("Se ha producido un error en guardar la tabla de pruebas", "" . $_);
+    $rpta{'mensaje'} = [@temp];
+  };
+  #print("\n");print Dumper(%rpta);print("\n");
+  return to_json \%rpta;
 };
 
 true;
---
+++ lib/Model/Prueba.pm
@@ -0,0 +1,45 @@
+package Model::Prueba;
+use Config::Database;
+
+sub new {
+  my $class = shift;
+  my $db = 'Config::Database';
+  my $odb= $db->new();
+  my $dbh = $odb->getConnection();
+  my $self = {
+    _dbh => $dbh
+  };
+  bless $self, $class;
+  return $self;
+}
+
+sub listar {
+  my($self, $subtitulo_id) = @_;
+  my $sth = $self->{_dbh}->prepare('SELECT id, nombres, paterno, materno, correo FROM pruebas') 
+    or die "prepare statement failed: $dbh->errstr()";
+  $sth->bind_param( 1, $subtitulo_id);
+  $sth->execute() or die "execution failed: $dbh->errstr()";
+  my @rpta;
+  while (my $ref = $sth->fetchrow_hashref()) {
+    push @rpta, $ref;
+  }
+  $sth->finish;
+  return @rpta;
+}
+
+sub crear {
+  my($self, $nombres, $paterno, $materno, $correo) = @_;
+  my $sth = $self->{_dbh}->prepare('INSERT INTO pruebas (nombres, paterno, materno, correo) VALUES (?, ?, ?, ?)') 
+    or die "prepare statement failed: $dbh->errstr()";
+  $sth->bind_param( 1, $nombres);
+  $sth->bind_param( 2, $paterno);
+  $sth->bind_param( 3, $materno);
+  $sth->bind_param( 4, $correo);
+  $sth->execute() or die "execution failed: $dbh->errstr()";
+  $sth->finish;
+  my $id_generated = $self->{_dbh}->last_insert_id(undef, undef, undef, undef );
+  #my $id_generated = $self->{_dbh}->firebird_insertid;
+  return $id_generated;
+}
+
+1;
